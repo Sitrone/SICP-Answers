0123a09074f8b8888f2df1a43872b3846166e939
Sitrone <sz1990524@gmail.com>
2015-01-20 14:51:13 +0800

    Update 2.40.scm

--- Chp2/2.40.scm
+++ Chp2/2.40.scm
@@ -1,10 +1,33 @@
-;;Supporting functions
-
-
-
+ ;; Supporting functions: 
+  
+ (define nil '()) 
+  
+ (define (filter predicate sequence) 
+   (cond ((null? sequence) nil) 
+         ((predicate (car sequence)) 
+          (cons (car sequence)  
+                (filter predicate (cdr sequence)))) 
+         (else (filter predicate (cdr sequence))))) 
+  
+ (define (accumulate op initial sequence) 
+   (if (null? sequence) 
+       initial 
+       (op (car sequence) 
+           (accumulate op initial (cdr sequence))))) 
+  
+ (define (enumerate-interval low high) 
+   (if (> low high) 
+       nil 
+       (cons low (enumerate-interval (+ low 1) high)))) 
+  
+  
+ (define (flatmap proc seq) 
+   (accumulate append nil (map proc seq))) 
+  
+ 
 ;;Reuse flatmap
 (define (uniq-pairs n)
-    (flatmap (lambda (x)
+    (flatmap (lambda (i)
                   (map (lambda (j) (list i j))
                         (enumerate-intervl 1 (- i 1))))
                     (enumerate-intervl 1 n)))
@@ -13,4 +36,12 @@
 
 
 ;;If we change conditons to this :
-;;
+;;distinct positive integers i , j less than or equal to a given n
+;; Untested
+
+(define (uniq-pairs-second n)
+    (flatmap (lambda (i)
+                    (map (lambda (j) (list i (- n i))
+                        (enumerate-interval 1 n))))
+                (enumerate-interval 1 n)))
+
