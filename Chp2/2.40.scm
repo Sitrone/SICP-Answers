cc2b2f5c6b2b3d7da1e59bfbba37c25597b012fd
Sitrone <sz1990524@gmail.com>
2015-01-20 10:58:44 +0800

    Update 2.40.scm

--- Chp2/2.40.scm
+++ Chp2/2.40.scm
@@ -1,2 +1,16 @@
+;;Supporting functions
 
-(define uniq
+
+
+;;Reuse flatmap
+(define (uniq-pairs n)
+    (flatmap (lambda (x)
+                  (map (lambda (j) (list i j))
+                        (enumerate-intervl 1 (- i 1))))
+                    (enumerate-intervl 1 n)))
+
+;;Test
+
+
+;;If we change conditons to this :
+;;
