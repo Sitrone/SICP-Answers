88493b81b361add594ce710be684cdbf2abfdc28
Sitrone <sz1990524@gmail.com>
2015-01-20 17:02:22 +0800

    Create 2.51.scm

---
+++ Chp2/2.51.scm
@@ -0,0 +1,4 @@
+[The difference between eq?, eqv?, equal?, and = in Scheme?](http://stackoverflow.com/questions/16299246/what-is-the-difference-between-eq-eqv-equal-and-in-scheme)
+
+(define (equal? a b)
+    
