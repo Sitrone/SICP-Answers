61491ec32a3478525b47b37aa559e4000c3f16e1
sitrone <sununiq@163.com>
2016-08-05 22:50:59 +0800

    go on

---
+++ Chp1/1.11.scm
@@ -0,0 +1,19 @@
+; ;;Recursive
+(define (f n)
+        (if (< n 3)
+          n
+          (+ (f (- n 1))
+                (* 2 (f (- n 2)))
+                (* 3 (f (- n 3))))))
+
+
+
+;iterative
+(define (f n)
+      (f-iter 2 1 0 n))
+(define (f-iter a b c count)
+      (cond ((= count 0) c)
+            ((= count 1) b)
+            ((= count 2) a)
+            ((> count 2)
+                (f-iter (+ a (* 2 b) (* 3 c)) a b (- count 1)))))
---
+++ Chp1/1.12.scm
@@ -0,0 +1,16 @@
+;; recursive
+(define (pascal r c)
+        (cond ((> c r) (error "error"))
+              ((or (= c 0) (= r c)) 1)
+              (else (+ (pascal (- r 1) (- c 1))
+                        (pascal (- r 1) c)))))
+
+;; iterable
+;;(x + y)^n 二项式定理
+(load "chp1\\factorial.scm")
+(define (pascal r c)
+        (/ (factorial r)
+          (* (factorial c)
+              (factorial (- r c)))))
+
+;; 树形展开的条件下面，所求参数越大，二者的求取速度差异越大
