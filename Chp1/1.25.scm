8f322343b153bb8c58da5d2029ef6f1253b6d4f1
sitrone <sununiq@163.com>
2016-08-07 12:44:06 +0800

    no message

---
+++ Chp1/1.25.scm
@@ -0,0 +1,9 @@
+(load "fast-expt.scm")
+
+(define (expmod base exp m)
+    (remainder (fast-expt base exp) m))
+
+
+;; Alyssa的算法在数比较小的时候可以计算，但是如果要检测非常大的数，就会超过计算机的限制
+;; 造成溢出，而书上34页的expmod函数，每次都对这一次的计算结果进行remainder计算，是的将
+;; 乘幂控制在一定的范围内，这样能够避免溢出的产生
---
+++ Chp1/fermat-test.scm
@@ -0,0 +1,24 @@
+(load "even-and-odd.scm")
+
+(define (square n)
+    (* n n))
+;; 模n同余（如果两个数除以n的余数相同）
+(define (expmod base exp m)
+    (cond ((= exp 0) 1)
+          ((even? exp)
+                    (remainder (square (expmod base (/ exp 2) m))
+                      m))
+          ((odd? exp)
+                    (remainder (* base (expmod base (- exp 1) m))
+                      m))))
+;; 选取1 - (n-1)之间的数a，检查a的n次幂取模n的余数是否等于a
+(define (fermat-test n)
+    (define (try-it a)
+        (= (expmod a n n) a))
+      (try-it (+ 1 (random (- n 1)))))
+
+;; 定义检测的次数
+(define (fast-prime? n times)
+    (cond ((= times 0) true)
+          ((fermat-test n) (fast-prime? n (- times 1)))
+          (else fasle)))
