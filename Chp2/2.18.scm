5ef2afd90f3d4e76eeeff3a3187793b112fd5547
sitrone <sununiq@163.com>
2016-08-14 16:26:27 +0800

    edit

--- Chp2/2.18.scm
+++ Chp2/2.18.scm
@@ -1,4 +1,4 @@
-(load "2.18.scm")
+(load "2.17.scm")
 
 ;; 直观的想法是下面这样子的
 (define (reverse-test l)
@@ -20,7 +20,8 @@
     (define (it-iter lat ans)
         (if (null? lat)
             ans
-            (it-iter (cdr ans) (cons (car lat) ans))))
+            (it-iter (cdr ans)
+                     (cons (car lat) ans))))
     (it-iter items nil))
 ;;比如说，对于列表 (list 3 2 1) 来说， iter 先组合起 (cons 1 '()) ，
 ;;然后是 (cons 2 (cons 1 '())) ，最后是 (cons 3 (cons 2 (cons 1'())))
--- Chp2/2.19.scm
+++ Chp2/2.19.scm
@@ -10,5 +10,15 @@
                         (first-denomination conin-values))
                     conin-values)))))
 
-(define (first-denomination item-kinds)
-    ())
+(define (first-denomination conin-values)
+    (car conin-values))
+(define (except-first-denomination conin-values)
+    (cdr conin-values))
+(define (no-more? conin-values)
+    (null? conin-values))
+
+; Testing
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+; 顺序不会影响找零结果，可以进行测试，采用逆序测试
