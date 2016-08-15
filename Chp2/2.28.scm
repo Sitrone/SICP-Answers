c587ee15f83ffffa38e1e6669225dd7f69d456ba
sitrone <sununiq@163.com>
2016-08-15 22:39:43 +0800

    no message

---
+++ Chp2/2.28.scm
@@ -0,0 +1,14 @@
+(load "map.scm")
+
+(define (fringe x)
+    (cond ((null? x) nil)
+          ((not (pair? x)) (list x))
+          (else
+              (append (fringe (car x))
+                      (fringe (cdr x))))))
+
+
+; Testing
+(define x (list (list 1 2) (list 3 4)))
+(fringe x)
+(fringe (list x x))
