24ab3058320c2c946a3c78a584ea3426da29a5e9
sitrone <sununiq@163.com>
2016-08-22 22:58:23 +0800

    图形语言

---
+++ Chp2/2.44.scm
@@ -0,0 +1,7 @@
+; 模仿right-split，很容易可以写出
+
+(define (up-split painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller (up-split painter (- n 1))))
+            (below painter (beside smaller smaller)))))
---
+++ Chp2/p88.scm
@@ -0,0 +1,26 @@
+; 图形语言的抽象
+(load "2.44.scm")
+
+(define (right-split)
+    (if (= n 0)
+        painter
+        (let ((smaller (right-split (- n 1))))
+          (beside painter (below smaller smaller)))))
+
+(define (corner-split painter n)
+    (if (= n 0)
+        painter
+        (let ((up (up-split painter (- n 1)))
+             (right (right-split painter (- n 1))))
+          (let ((top-left (beside up up))
+                (bottom-right (below right right))
+                (corner (corner-split painter (- n 1))))
+            (beside (below painter top-left)
+                    (below bottom-right corner))))))
+
+(define (spuare-limit painter n)
+    (let ((quarter (corner-split painter n)))
+      (let ((half (beside (flip-horiz quarter) quarter)))
+          (below (flip-vert half) half))))
+
+  
