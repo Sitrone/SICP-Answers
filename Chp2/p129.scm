afb2f6bbc40bc27f955e4684df08482773fa5fdc
sitrone <sununiq@163.com>
2016-09-03 20:34:03 +0800

    带有通用型的操作系统

--- Chp2/p129.scm
+++ Chp2/p129.scm
@@ -1,17 +1,32 @@
+(load "p186-get-put.scm")
+(load "p124-polar.scm")
+(load "p123-rectangular.scm")
 
 (define (add x y) (apply-generic 'add x y))
 (define (sub x y) (apply-generic 'sub x y))
 (define (mul x y) (apply-generic 'mul x y))
 (define (div x y) (apply-generic 'div x y))
 
-;; 定义过程attach-tag和contents
+;; 标识（tag）处理函数：
 (define (attach-tag type-tag contents)
     (cons type-tag contents))
+
 (define (type-tag datum)
     (if (pair? datum)
         (car datum)
-        (error "wrong -- TYPE TAG" datum)))
+        (error "Bad tagged datum -- TYPE-TAG" datum)))
+
 (define (contents datum)
     (if (pair? datum)
         (cdr datum)
-        (error "wrong -- CONTENTS" datum)))
+        (error "Bad tagged datum -- CONTENTS" datum)))
+
+; apply-generic 函数
+(define (apply-generic op . args)
+    (let ((type-tags (map type-tag args)))
+        (let ((proc (get op type-tags)))
+            (if proc
+                (apply proc (map contents args))
+                (error
+                    "No method for these types -- APPLY-GENERIC"
+                    (list op type-tags))))))
--- Chp2/p130-rational-package.scm
+++ Chp2/p130-rational-package.scm
@@ -2,42 +2,42 @@
 (load "p129.scm")
 
 (define (install-rational-package)
-;; interal procedures
-(define (number x) car x)
-(define (demon x) cdr x)
-(define (make-rat n d)
-    (let ((g (gcd n d)))
-      (cons (/ n g) (/ d g))))
+    ;; interal procedures
+    (define (number x) car x)
+    (define (demon x) cdr x)
+    (define (make-rat n d)
+        (let ((g (gcd n d)))
+          (cons (/ n g) (/ d g))))
 
-;; add-rat
-(define (add-rat x y)
-    (make-rat (+ (* (number x) (denom y))
-                 (* (number y) (denom x)))
-              (* (denom x) (denom y))))
-;; sub-rat
-(define (sub-rat x y)
-    (make-rat (- (* (number x) (demon y))
-                 (* (number y) (demon x)))
-              (* (demon x) (demon y))))
-;; mul-rat
-(define (mul-rat x y)
-      (make-rat (* (number x) (number y))
-                (* (demon x) (demon y))))
-;;div-rat
-(define (div-rat x y)
-      (make-rat (* (number x) (demon y))
-                (* (number y) (demon x))))
+    ;; add-rat
+    (define (add-rat x y)
+        (make-rat (+ (* (number x) (denom y))
+                     (* (number y) (denom x)))
+                  (* (denom x) (denom y))))
+    ;; sub-rat
+    (define (sub-rat x y)
+        (make-rat (- (* (number x) (demon y))
+                     (* (number y) (demon x)))
+                  (* (demon x) (demon y))))
+    ;; mul-rat
+    (define (mul-rat x y)
+          (make-rat (* (number x) (number y))
+                    (* (demon x) (demon y))))
+    ;;div-rat
+    (define (div-rat x y)
+          (make-rat (* (number x) (demon y))
+                    (* (number y) (demon x))))
 
-;; interface to the rest of the system
-(define (tag x) (attach-tag 'rational x))
-(define 'add '(rational rational)
-    (lambda (x y) (tag (add-rat x y))))
-(define 'sub '(rational rational)
-    (lambda (x y) (tag (sub-rat x y))))
-(define 'mul '(rational rational)
-    (lambda (x y) (tag (mul-rat x y))))
-(define 'div '(rational rational)
-    (lambda (x y) (tag (div-rat x y))))
-(define 'make 'rational
-    (lambda (n d) (tag (make-rat n d))))
+    ;; interface to the rest of the system
+    (define (tag x) (attach-tag 'rational x))
+    (put 'add '(rational rational)
+        (lambda (x y) (tag (add-rat x y))))
+    (put 'sub '(rational rational)
+        (lambda (x y) (tag (sub-rat x y))))
+    (put 'mul '(rational rational)
+        (lambda (x y) (tag (mul-rat x y))))
+    (put 'div '(rational rational)
+        (lambda (x y) (tag (div-rat x y))))
+    (put 'make 'rational
+        (lambda (n d) (tag (make-rat n d))))
 'done)
