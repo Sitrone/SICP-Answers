0c31951bea5f60088c08ac74e447e6b6278aec41
sitrone <sununiq@163.com>
2016-09-29 22:25:25 +0800

    queue

---
+++ Chp3/3.21.scm
@@ -0,0 +1,16 @@
+(load "queue.scm")
+
+;(define q1 (make-queue))
+;(insert-queue! q1 'a)
+;(display q1)
+;(insert-queue! q1 'b)
+;(display q1)
+;(delete-queue! q1)
+;(display q1)
+;(delete-queue! q1)
+;(display q1)
+
+(define (print-queue queue)
+  (car queue))
+
+;; 解释器打印((a b) b)实际上是把car和cdr部分都打印出来了，很容易造成误解
